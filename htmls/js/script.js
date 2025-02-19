810c7fa6c8f776a0a90e746428000eeac441a78f
firdBro <154615244+firdBro@users.noreply.github.com>
2025-02-19 22:28:13 +0500

    Delete htmls directory

--- htmls/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const buyButtons = document.querySelectorAll("button");
-    
-    buyButtons.forEach(button => {
-        button.addEventListener("click", function() {
-            window.location.href = "https://t.me/firdBro";
-        });
-    });
-});
